4b5fb14be7350fc82d6c89a7c45bf6f73d7462d6
Neil Grogan <neil@grogan.ie>
2019-01-22 17:46:57 +0000

    Added more news sources, making more generic

--- server.py
+++ server.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-import urllib.request
+from urllib.request import urlopen
+from urllib.parse import urlparse
+import configparser
 from bs4 import BeautifulSoup
 from flask import Flask, render_template
+from email.utils import formatdate
 
 app = Flask(__name__)
+
 
 def get_data(url):
     """
     Performs a GET request & returns the content with BS
     """
-    result = urllib.request.urlopen(url).read()
+    result = urlopen(url).read()
     return BeautifulSoup(result, "html.parser")
 
-def get_headlines():
-    URL = 'https://www.irishtimes.com' # TODO: Sample url
-    soup = get_data(URL)
+def get_headlines(url, pub_date):
+    url = urlparse(url).geturl()
+    soup = get_data(url)
+    articles = []
     for section in soup.select("div.trendingarticles > div.story"):
-        print('Headline:',section.find("span", class_="tr-headline").get_text())
-        print('URL:', URL + section.find("a").get("href"))
+        d = {}
+        d['title'] = section.find("span", class_="tr-headline").get_text()
+        d['url'] = URL + section.find("a").get("href")
+        d['pub_date'] = pub_date
+        articles.append(d)
+    return articles
+
+
+def get_site(site_key):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    conf = config[site_key]
+
+    site = {}
+    site['title'] = conf['title']
+    site['url'] = conf['url']
+    site['pub_date'] = formatdate()
+    site['articles'] = get_headlines(str(conf['url']), formatdate())
+    
+    return site
 
 @app.route("/")
 def index():
@@ -27,9 +50,8 @@ def index():
 
 @app.route("/<news_source>.rss")
 def get_rss_feed(news_source):
-    return render_template('index.rss', feeds=[])
+    return render_template('index.rss', site=get_site(news_source))
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-    get_headlines()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
