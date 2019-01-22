55d0b70f2baa9c80f7fbcac81898a6949e431234
Neil Grogan <neil@grogan.ie>
2019-01-22 16:43:18 +0000

    Fetching headlines, start of config

--- news2rss.py
+++ news2rss.py
@@ -1,32 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import requests
+import urllib.request
 from bs4 import BeautifulSoup
 
-def get_data(session, url):
+def get_data(url):
 	"""
 	Performs a GET request & returns the content with BS
 	"""
-	result = session.get(url)
-	return BeautifulSoup(result.content, "html.parser")
-
-def post_data(session, url, data):
-	"""
-	Performs a POST request & returns the content with BS
-	"""
-	result = session.post(url, data=data, headers=dict(referer=CONF['LOGIN_URL']))
-	return BeautifulSoup(result.content, "html.parser")
+	result = urllib.request.urlopen(url).read()
+	return BeautifulSoup(result, "html.parser")
 
 
 def main():
   """
-	Main Method if running on the CLI
-	"""
+  Main Method if running on the CLI
+  """
   URL = 'https://www.irishtimes.com' # TODO: Sample url
-  session = requests.session()
-  soup = get_data(session, URL)
-  ta_section = soup.find("div", class_="trendingarticles")
-  for section in ta_section.find_all("div", class_="story"):
+  soup = get_data(URL)
+  for section in soup.select("div.trendingarticles > div.story"):
     print('Headline:',section.find("span", class_="tr-headline").get_text())
     print('URL:', URL + section.find("a").get("href"))
 
