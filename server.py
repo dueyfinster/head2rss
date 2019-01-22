267cce1bb0fd3a8cf73d89be26accd2f7d809cd7
Neil Grogan <neil@grogan.ie>
2019-01-22 15:49:21 +0000

    Start of server

---
+++ server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html', feeds=[])
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
